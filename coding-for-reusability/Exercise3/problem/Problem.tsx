81d572ef37a3246e97cfce377a144ad53371e16f
Christina Labay <christina@bitovi.com>
2023-08-17 12:19:38 -0500

    update problem 3

--- coding-for-reusability/Exercise3/problem/Problem.tsx
+++ coding-for-reusability/Exercise3/problem/Problem.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 /*
 Below we have sortArrayOfObjectsByKey. Currently, all types of the function parameters are any.
 
@@ -18,3 +19,51 @@ Update SortList and SortListProps similarly, except constrain
  each item to also have an id property that must be either string or number. 
  This will be the specified key that we sort by.
 */
+
+function sortArrayOfObjectsByKey(arr, key, sortOrder) {
+    return arr.sort((a, b) => {
+      const _a = a[key];
+      const _b = b[key];
+  
+      if (typeof _a === "string" && typeof _b === "string") {
+        return _a.localeCompare(_b) * sortOrder;
+      }
+  
+      if (typeof _a === "number" && typeof _b === "number") {
+        return (_a - _b) * sortOrder;
+      }
+  
+      return 0;
+    });
+  }
+  
+  const users = [
+    { id: 1, name: "John", age: 21 },
+    { id: 2, name: "Mary", age: 23 },
+    { id: 3, name: "Andrew", age: 20 },
+  ];
+  
+  /*
+    const sortedArrOfObjects = sortArrayOfObjectsByKey(users, "age", 1);
+    console.log(sortedArrOfObjects); // [{ name: 'Andrew', age: 20 }, { name: 'John', age: 21 }, { name: 'Mary', age: 23 }]
+  */
+  
+  interface SortListProps {
+    list: /* implementation details */
+    sortKey: /* implementation details */
+    sortOrder: /* implementation details */
+  }
+  
+  function SortList({ list, sortKey, sortOrder }) {
+    return (
+      <ul>
+        {sortArrayOfObjectsByKey(list, sortKey, sortOrder).map((item) => (
+          <li key={item.id}>{JSON.stringify(item)}</li>
+        ))}
+      </ul>
+    );
+  }
+  
+  function SortedUsers() {
+    return <SortList list={users} sortKey="age" sortOrder={1} />;
+  }
