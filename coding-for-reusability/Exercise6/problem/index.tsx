0dfd9c1fc9509979f2e9039f4dfddbaa60c452d9
Christina Labay <christina@bitovi.com>
2023-09-05 19:03:57 -0500

    test

--- coding-for-reusability/Exercise6/problem/index.tsx
+++ coding-for-reusability/Exercise6/problem/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 /*
 Below we have sortArrayOfObjectsByKey. Currently, all types of the function parameters are implicitly any.
 
@@ -20,68 +20,66 @@ SortListProps should take one type argument that extends an Object that has an i
 
 //1)Update sortArrayOfObjectsByKey to be a generic function that takes in two type arguments.
 function sortArrayOfObjectsByKey(itemList, key, sortOrder) {
-    return itemList.sort((first, second) => {
-      const firstItem = first[key];
-      const secondItem = second[key];
-  
-      if (typeof firstItem === "string" && typeof secondItem === "string") {
-        return firstItem.localeCompare(secondItem) * sortOrder;
-      }
-  
-      if (typeof firstItem === "number" && typeof secondItem === "number") {
-        return (firstItem - secondItem) * sortOrder;
-      }
-  
-      return 0;
-    });
-  }
-  
-  
-  const users = [
-    { id: 1, name: "John", age: 21 },
-    { id: 2, name: "Mary", age: 23 },
-    { id: 3, name: "Andrew", age: 20 },
-  ];
-  
-  /*
+  return itemList.sort((first, second) => {
+    const firstItem = first[key];
+    const secondItem = second[key];
+
+    if (typeof firstItem === "string" && typeof secondItem === "string") {
+      return firstItem.localeCompare(secondItem) * sortOrder;
+    }
+
+    if (typeof firstItem === "number" && typeof secondItem === "number") {
+      return (firstItem - secondItem) * sortOrder;
+    }
+
+    return 0;
+  });
+}
+
+const users = [
+  { id: 1, name: "John", age: 21 },
+  { id: 2, name: "Mary", age: 23 },
+  { id: 3, name: "Andrew", age: 20 },
+];
+
+/*
   2) Update to a generic, constraining each item to also have an id property that must be either string or number.
   */
-  interface SortListProps {
-    list: /* TODO: implementation details */
-    sortKey: /* TODO: implementation details */
-    sortOrder: /* TODO: implementation details */
-  }
+interface SortListProps {
+  list: unknown /* TODO: implementation details */;
+  sortKey: unknown /* TODO: implementation details */;
+  sortOrder: unknown /* TODO: implementation details */;
+}
 
-  /*
+/*
   2) Also update to a generic, constraining each item to also have an id property that must be either string or number.
   Use SortListProps as the props type
   */
-  function SortList({ list, sortKey, sortOrder }) {
-    return (
-      <ul>
-        {sortArrayOfObjectsByKey(list, sortKey, sortOrder).map((item) => (
-          <li key={item.id}>{JSON.stringify(item)}</li>
-        ))}
-      </ul>
-    );
-  }
-  
-  function SortedUsers() {
-    return <SortList list={users} sortKey="age" sortOrder={1} />;
-  }
-
-  //In app code:
-  <SortedUsers />
-
-  
-  //Example usage
-  sortArrayOfObjectsByKey(users, 'name', -1);
-  sortArrayOfObjectsByKey(
-    [
-      { brand: "Expo", product: "marker", id: 1 },
-      { brand: "Swingline", product: "stapler", id: 2 },
-    ],
-    "brand",
-    1
+function SortList({ list, sortKey, sortOrder }) {
+  return (
+    <ul>
+      {sortArrayOfObjectsByKey(list, sortKey, sortOrder).map((item) => (
+        <li key={item.id}>{JSON.stringify(item)}</li>
+      ))}
+    </ul>
   );
-  sortArrayOfObjectsByKey(users, 'product', -1);//This should error, because product does not exist in users
+}
+
+function SortedUsers() {
+  return <SortList list={users} sortKey="age" sortOrder={1} />;
+}
+
+//In app code:
+<SortedUsers />;
+
+//Example usage
+sortArrayOfObjectsByKey(users, "name", -1);
+sortArrayOfObjectsByKey(
+  [
+    { brand: "Expo", product: "marker", id: 1 },
+    { brand: "Swingline", product: "stapler", id: 2 },
+  ],
+  "brand",
+  1
+);
+sortArrayOfObjectsByKey(users, "product", -1); //This should error, because product does not exist in users
