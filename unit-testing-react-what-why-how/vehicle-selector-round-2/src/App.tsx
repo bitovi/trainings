2622df1e3ee06a1fdc1165a69463e5a434ef5bac
Josh Glantz-Hucks <65362632+Jahteo@users.noreply.github.com>
2023-09-27 09:57:33 -0700

    foo

--- unit-testing-react-what-why-how/vehicle-selector-round-2/src/App.tsx
+++ unit-testing-react-what-why-how/vehicle-selector-round-2/src/App.tsx
@@ -8,6 +8,8 @@ function App() {
 			<iframe
 				title="vehicle-selector"
 				src="https://bitovi.github.io/vehicle-selector-react/"
+				width="90%"
+				height="90%"
 			></iframe>
 		</div>
 	);
