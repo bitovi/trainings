f224ca1a6d73bd41ff9e42187e6cf2116e637c67
Josh Glantz-Hucks <65362632+Jahteo@users.noreply.github.com>
2023-09-26 19:18:07 -0700

    test

--- unit-testing-react-what-why-how/Basics/solution/src/index.test.js
+++ unit-testing-react-what-why-how/Basics/solution/src/index.test.js
@@ -1,15 +1,38 @@
-import { helloWorld } from "./index";
-import { isColorInArray } from "./index";
+import { helloWorld, isColorInArray } from "./index";
 
-// 1. What does this test for? What purpose does it serve?
+// 1. What does this test?
+// Smoke test to see if `helloWorld` doesn't immediately error.
+// Also tests that codesandbox can run tests successfully.
+// Does not test if `helloWorld` works properly. 
+// Does not test if `helloWorld` returns a string or what that string is.
 
+// Better test name: "passes smoke test" or "is not on fire"
 describe("hello world", () => {
-	test("should not fail immediately", () => {
-		const result = helloWorld();
-		expect(result);
+		test("Badly named initial test", () => {
+
+			const result = helloWorld();
+
+			expect(result);
 	});
 });
 
 // 2. Write a smoke test for the `isColorInArray` function
+describe("isColorInArray", () => {
+		test("is not on fire", () => {
+
+			const result = isColorInArray({string: "red", colors: ["red", "blue", "green"]});
+			
+			expect(result);
+	});
+});
 
 // 3. Write a comprehensive test for the `isColorInArray` function
+describe("isColorInArray", () => {
+		test("returns true when `potentialColor` is in `colors`", () => {
+
+			const result = isColorInArray({potentialColor: "red", colors: ["red", "blue", "green"]});
+
+			// `.toBe(true)`
+			expect(result).toBe(true);
+	});
+});
