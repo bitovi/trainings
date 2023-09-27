5bf77391337600a962510bbbe1b37d3b4aecb761
Josh Glantz-Hucks <65362632+Jahteo@users.noreply.github.com>
2023-09-27 10:45:22 -0700

    ahoy matey

--- unit-testing-react-what-why-how/example-jest-react/testing-with-jest/src/App.tsx
+++ unit-testing-react-what-why-how/example-jest-react/testing-with-jest/src/App.tsx
@@ -1,12 +1,10 @@
 function App() {
 	return (
 		<div>
-			<p>Landing tada</p>
+			<p>Landing tada sans cow</p>
 			<iframe
 				title="vehicle-selector"
 				src="https://bitovi.github.io/vehicle-selector-react/"
-				width="600"
-				height="800"
 			></iframe>
 		</div>
 	);
