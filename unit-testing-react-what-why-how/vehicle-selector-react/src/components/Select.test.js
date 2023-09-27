63a36fef2fd0be59052478538afdedb082bd227c
Josh Glantz-Hucks <65362632+Jahteo@users.noreply.github.com>
2023-09-26 20:29:20 -0700

    test

---
+++ unit-testing-react-what-why-how/vehicle-selector-react/src/components/Select.test.js
@@ -0,0 +1,10 @@
+import { Select } from "../src/index"
+import { render, screen } from '@testing-library/react'
+
+test("<Select/> renders successfully", () => {
+    render(<Select/>);
+
+    const element = screen.getByText(/Select.../i);
+
+    expect(element).toBeInTheDocument();
+})
