54ba9d19e165173fa68e658244f38a6bb4d45db2
Josh Glantz-Hucks <65362632+Jahteo@users.noreply.github.com>
2023-09-27 09:54:57 -0700

    foo

--- unit-testing-react-what-why-how/vehicle-selector-round-2/src/App.tsx
+++ unit-testing-react-what-why-how/vehicle-selector-round-2/src/App.tsx
@@ -1,43 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-import bitoviLogo from "@assets/bitovi-logo.svg";
-import {
-	VehicleSelector,
-	VehicleSelectorWithEffects,
-	VehicleSelectorBetterUseState,
-	VehicleSelectorUseReducer,
-} from "./components/vehicleSelector";
-import vehicleSelectorCode from "@components/vehicleSelector/VehicleSelector?raw";
-import vehicleSelectorWithEffectsCode from "@components/vehicleSelector/VehicleSelectorWithEffects?raw";
-import vehicleSelectorBetterUseStateCode from "@components/vehicleSelector/VehicleSelectorBetterUseState?raw";
-import vehicleSelectorUseReducerCode from "@components/vehicleSelector/VehicleSelectorUseReducer?raw";
-import { Highlighter } from "rc-highlight";
-
-type Version =
-	| "useState"
-	| "useStateWithEffects"
-	| "useStateBetter"
-	| "useReducer";
-
-const VehicleSelectors: Record<Version, JSX.Element> = {
-	useState: <VehicleSelector />,
-	useStateWithEffects: <VehicleSelectorWithEffects />,
-	useStateBetter: <VehicleSelectorBetterUseState />,
-	useReducer: <VehicleSelectorUseReducer />,
-};
-
-const VehicleSelectorCode: Record<Version, string> = {
-	useState: vehicleSelectorCode,
-	useStateWithEffects: vehicleSelectorWithEffectsCode,
-	useStateBetter: vehicleSelectorBetterUseStateCode,
-	useReducer: vehicleSelectorUseReducerCode,
-};
 
 function App() {
-	const [version, setVersion] = useState<Version>("useState");
-
 	return (
-		<div className="app">
+		<div>
+			<p>Iframe of working site here: emptied app.tsx</p>
 			<iframe
 				title="vehicle-selector"
 				src="https://bitovi.github.io/vehicle-selector-react/"
