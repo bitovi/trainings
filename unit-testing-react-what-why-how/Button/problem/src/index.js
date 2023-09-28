443f586e87469f5ab120502da45e78c2144862c1
Josh Glantz-Hucks <65362632+Jahteo@users.noreply.github.com>
2023-09-28 15:32:04 -0700

    minus mock & msw

--- unit-testing-react-what-why-how/Button/problem/src/index.js
+++ unit-testing-react-what-why-how/Button/problem/src/index.js
@@ -2,23 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-import { worker } from "./mocks/worker";
 
 if (process.env.NODE_ENV === "development") {
-  worker
-    .start({
-      serviceWorker: {
-        url: "/mockServiceWorker.js",
-      },
-    })
-    .then(() => {
-      const rootElement = document.getElementById("root");
-      const root = createRoot(rootElement);
+	const rootElement = document.getElementById("root");
+	const root = createRoot(rootElement);
 
-      root.render(
-        <StrictMode>
-          <App />
-        </StrictMode>
-      );
-    });
+	root.render(
+		<StrictMode>
+			<App />
+		</StrictMode>
+	);
 }
