4e336af8b12ca46559d40732543c64951263e069
Josh Glantz-Hucks <65362632+Jahteo@users.noreply.github.com>
2023-09-29 08:57:31 -0700

    renderHook

--- unit-testing-react-what-why-how/renderHook/problem/src/index.test.js
+++ unit-testing-react-what-why-how/renderHook/problem/src/index.test.js
@@ -1,8 +1,8 @@
-// TODO: Convert this solution to a problem. I think it's supposed to be made from scratch?
-// renderHook’s return value is actually the “result.current”
-// useLoggedInUser will always return the same, but for simplicity sake, pretend it’s a good hook.
+import {useLoggedInUser} from './index.js'
+
+// note: renderHook’s return value is actually the “result.current”
+
 test('returns logged in user', () => {
-  const {result} = renderHook(() => useLoggedInUser())
-  expect(result.current).toEqual({name: 'Alice'})
+// write tests here...
 })
 
--- unit-testing-react-what-why-how/renderHook/solution/src/index.test.js
+++ unit-testing-react-what-why-how/renderHook/solution/src/index.test.js
@@ -1,7 +1,9 @@
-// TODO: test this
+import {useLoggedInUser} from './index.js'
+
 // renderHook’s return value is actually the “result.current”
-// useLoggedInUser will always return the same, but for simplicity sake, pretend it’s a good hook.
+
 test('returns logged in user', () => {
   const {result} = renderHook(() => useLoggedInUser())
   expect(result.current).toEqual({name: 'Alice'})
 })
+
