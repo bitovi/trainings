72557dab5f6449bd38c0292e0807e934a2379c38
Josh Glantz-Hucks <65362632+Jahteo@users.noreply.github.com>
2023-10-02 15:18:49 -0700

    cleanup Basics-together exercises

--- unit-testing-react-what-why-how/Basics-together/solution/src/index.test.js
+++ unit-testing-react-what-why-how/Basics-together/solution/src/index.test.js
@@ -1,34 +1,31 @@
-import {sum} from "./index" /// Potential Solutions.
+import { sum } from "./index"; 
 describe("the sum function", () => {
+	// Test broken into AAA pattern
+	it("adds the numbers together", () => {
+		// Assign
+		const values = [1, 2];
 
-  // Test broken into AAA pattern
-  it("adds the numbers together", () => {
-    
-    // Assign
-    const values = [ 1, 2]
-    
-    // Act
-    const result = sum(values)
-    
-    // Assert
-    expect(result).toBe(3)
-  });
-  
-  // Condensed tests:
+		// Act
+		const result = sum(values);
 
-  // Smoke Test
-  it("isn't on fire 🔥", () => {
-    sum([ 1, 2 ])
-  })
+		// Assert
+		expect(result).toBe(3);
+	});
 
-  // Contract Test
-  it("returns a number", () => {
-    expect(typeof sum([ 1, 2 ])).toBe("number")
-  })
+	// Condensed tests:
 
-  // Comprehensive Test
-  it("adds the numbers", () => {
-    expect(sum([ 1, 2 ])).toBe(3)
-  })
+	// Smoke Test
+	it("isn't on fire 🔥", () => {
+		sum([1, 2]);
+	});
 
-}) 
+	// Contract Test
+	it("returns a number", () => {
+		expect(typeof sum([1, 2])).toBe("number");
+	});
+
+	// Comprehensive Test
+	it("adds the numbers", () => {
+		expect(sum([1, 2])).toBe(3);
+	});
+});
