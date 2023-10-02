d53ccd73254646c58d0de48d1abaf18bf55cc2c9
Chasen Le Hara <chasen@bitovi.com>
2023-10-01 18:22:05 -0700

    Remove an extra folder with a space in its name
    This removes the `fractal-modlets/Exercise1/problem /` folder. I think it was just a duplicate of the correct `problem` folder.

--- fractal-modlets/Exercise1/problem /src/components/ImageCarousel.tsx
+++
@@ -1,7 +0,0 @@
-//import styles from "../styles/ImageCarousel.module.css"
-
-const ImageCarousel = () => {
-  return <div>This is an image carousel component!</div>;
-};
-
-export default ImageCarousel
