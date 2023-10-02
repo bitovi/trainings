85061412dfb7adb9739e99e95900bb4c10340ee1
Chasen Le Hara <chasen@bitovi.com>
2023-10-02 15:35:26 -0700

    Flesh out the renderHook test
    This includes a different “hook” to test (which could still be improved upon) and the necessary infrastructure to run the tests locally.

--- unit-testing-react-what-why-how/renderHook/problem/src/index.tsx
+++
@@ -1,4 +0,0 @@
-// useLoggedInUser will always return the same, but for simplicity sake, pretend it’s a good hook.
-export default function useLoggedInUser() {
-	return { name: "Alice", email: "envkt@example.com" };
-}
