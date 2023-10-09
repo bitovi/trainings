f44442ce6ff92ea1535a8f486d279da03eac65ad
Chasen Le Hara <chasen@bitovi.com>
2023-10-08 18:45:34 -0700

    Improve the “Typing React Events” exercise
    This:
    
    - Rewords the comments to prompt participants to finish the exercise
    - Refactors the JSX to use semantic HTML
    
    I used `FormEvent` and I’m not sure if that means we should also cover this during the training.

--- typing-react-events/Exercise1/problem/src/App.tsx
+++ typing-react-events/Exercise1/problem/src/App.tsx
@@ -1,51 +1,61 @@
-// 1. Add a type to the event argument in the handleFieldChange
-//    function which would allow it to handle the event object 
-//    from both the fields and get rid of the TypeScript error
-// 2. Create an element which will call submitClickHandler 
-//    when the user clicks on this element, while keeping
-//    in mind the type defined for submitClickHandler. 
+/*
 
+Exercise:
 
-import React, { useState, MouseEvent } from 'react';
+1) Fix the `event` argument’s type for `buttonOnClickHandler`.
+2) Add a <button> that can trigger `formOnSubmitHandler`.
+3) Fix the `event` argument’s type for `handleChange`.
+
+*/
+
+import React, { FormEvent, useState } from 'react';
 
 const App = () => {
   const [fields, setFields] = useState({
+    description: "",
     title: "",
-    description: ""
   });
 
-  const handleFieldChange = (e) => {
-    // Parameter 'e' implicitly has an 'any' type.
+  function buttonOnClickHandler(event: never) {
+    alert(`Button received event with type and coordinates: ${event.type} (${event.clientX}, ${event.clientY})`);
+  }
 
-    const { name, value } = e.target;
+  function formOnSubmitHandler(event: FormEvent) {
+    event.preventDefault();
+
+    alert(`Form received event with type: ${event.type}`);
+  }
+
+  function handleChange(event: never) {
     setFields({
       ...fields,
-      [name]: value
+      [event.target.name]: event.target.value
     });
-  };
-
-  const submitClickHandler = (e: MouseEvent<HTMLDivElement>) => {
-    console.log("Submitted!");
-  };
+  }
 
   return (
-    <>
-      <div>
-        Title:
-        <input name="title" onChange={handleFieldChange}/>
-      </div>
-      <div>
-        Description:
-        <div>
-          <textarea name="description" onChange={handleFieldChange}/>
-        </div>
-      </div>
-      <div>
-     
-        {/* Submit element code goes here */}
-       
-      </div>
-    </>
+    <form onSubmit={formOnSubmitHandler}>
+      <p>
+        <label>
+          Title:
+          <input name="title" onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Description:
+          <textarea name="description" onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <button onClick={buttonOnClickHandler} type="button">
+          Submit (<code>type="button"</code>)
+        </button>
+      </p>
+      <p>
+        {/* Add a button to submit the form here. */}
+      </p>
+    </form>
   );
 }
 
