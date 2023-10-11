af4a6f9478bc068603bf86bea9f6a83a89a3ce70
Chasen Le Hara <chasen@bitovi.com>
2023-10-11 14:11:17 -0700

    Add @axe-core/react to the “Incorporating Accessibility” demo
    This allows developers to see axe issues when they open up their browser’s DevTools.

--- incorporating-accessibility-into-your-dev-process/demo/src/index.tsx
+++ incorporating-accessibility-into-your-dev-process/demo/src/index.tsx
@@ -1,5 +1,6 @@
+import axe from "@axe-core/react";
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM, { createRoot } from "react-dom/client";
 
 import App from "./App";
 
@@ -7,6 +8,7 @@ const mainElement = document.querySelector("main");
 if (mainElement) {
     const root = createRoot(mainElement);
     root.render(<App />);
+    axe(React, ReactDOM, 1000);
 } else {
     console.error("No main element");
 }
