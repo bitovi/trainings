be81658efc52b4cd83f616a99d575a0a505dcd27
Chasen Le Hara <chasen@bitovi.com>
2023-10-12 15:26:59 -0700

    Pretty print the axe results in the “IncorporatingAccessibility” demo
    This adds a package that nicely formats the results of the axe test.
    
    <details>
    <summary>Example output</summary>
    
    ```
     FAIL  src/App.test.tsx
      ● Console
    
        console.info
          Axe core library found 14 violations for the http://localhost/
    
          at prettyPrintAxeReport (node_modules/axe-result-pretty-print/src/index.ts:15:17)
    
        console.log
          ┌─────────┬──────────────────────────────────────────────────────────────────────────────────────────────┬──────────────────────────────┬────────────────────┬────────────┬───────┐
          │ (index) │                                         description                                          │              id              │        wcag        │   impact   │ nodes │
          ├─────────┼──────────────────────────────────────────────────────────────────────────────────────────────┼──────────────────────────────┼────────────────────┼────────────┼───────┤
          │    0    │                         'Ensures every id attribute value is unique'                         │        'duplicate-id'        │ 'WCAG 2.0 Level A' │  'minor'   │   1   │
          │    1    │        'Ensures <img> elements have alternate text or a role of none or presentation'        │         'image-alt'          │ 'WCAG 2.0 Level A' │ 'critical' │   2   │
          │    2    │                           'Ensures every form element has a label'                           │           'label'            │ 'WCAG 2.0 Level A' │ 'critical' │   6   │
          │    3    │                     'Ensures the document has at most one main landmark'                     │ 'landmark-no-duplicate-main' │  'Best practice'   │ 'moderate' │   1   │
          │    4    │ 'Landmarks should have a unique role or role/label/title (i.e. accessible name) combination' │      'landmark-unique'       │  'Best practice'   │ 'moderate' │   2   │
          │    5    │                            'Ensures links have discernible text'                             │         'link-name'          │ 'WCAG 2.0 Level A' │ 'serious'  │   1   │
          │    6    │                  'Ensures tabindex attribute values are not greater than 0'                  │          'tabindex'          │  'Best practice'   │ 'serious'  │   1   │
          └─────────┴──────────────────────────────────────────────────────────────────────────────────────────────┴──────────────────────────────┴────────────────────┴────────────┴───────┘
    
          at prettyPrintAxeReport (node_modules/axe-result-pretty-print/src/index.ts:19:25)
    ```
    
    </details>

--- incorporating-accessibility-into-your-dev-process/demo/src/App.test.tsx
+++ incorporating-accessibility-into-your-dev-process/demo/src/App.test.tsx
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
+import { prettyPrintAxeReport } from 'axe-result-pretty-print';
 import { axe, toHaveNoViolations } from 'jest-axe';
 
 import App from "./App";
@@ -10,6 +11,7 @@ describe('App component', () => {
     it('should not have any axe violations', async () => {
         const { container } = render(<App />);
         const results = await axe(container);
+        prettyPrintAxeReport(results);
         expect(results).toHaveNoViolations();
     });
 });
