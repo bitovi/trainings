f9aade5563de234d8cc6f4faa1943f23406f8694
Christina Labay <christina@bitovi.com>
2023-10-18 17:35:18 -0500

    breakout 5 and 6

---
+++ practical-react-unit-testing-with-jest-mocks-and-spies/Exercise5/solution/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Vehicle Information</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
