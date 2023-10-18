e20fbf3cb83251e8f2519451a3723739749ed97c
Christina Labay <christina@bitovi.com>
2023-10-18 14:05:22 -0500

    break out Exercise1

--- practical-react-unit-testing-with-jest-mocks-and-spies/ExerciseOne/problem/src/__mocks__/toSelectOptions.ts
+++ practical-react-unit-testing-with-jest-mocks-and-spies/ExerciseOne/problem/src/__mocks__/toSelectOptions.ts
@@ -1,3 +1,4 @@
 //Example 1) Manual Mock.
 
-module.exports = [{label: "2021", value: "2021"}, {label: "2022", value: "2022"}, {label: "2023", value: "2023"}]
+//export a mock value that is similar to the data toSelectOptions return value.
+module.exports = []
--- practical-react-unit-testing-with-jest-mocks-and-spies/ExerciseOne/solution/src/toSelectOptions.test.tsx
+++ practical-react-unit-testing-with-jest-mocks-and-spies/ExerciseOne/solution/src/toSelectOptions.test.tsx
@@ -2,9 +2,8 @@ const toSelectOptions = require("./toSelectOptions");
 
 jest.mock("./toSelectOptions");
 
-//the mock is located in the __mocks__ sibling folder
 describe("Manually mock and bypass toSelectOptions", () => {
-  test("Manual Mock test of toSelectOptions", () => {
+  test("Manual Mocking test of toSelectOptions", () => {
     expect(toSelectOptions).toEqual([
       { label: "2021", value: "2021" },
       { label: "2022", value: "2022" },
@@ -13,9 +12,7 @@ describe("Manually mock and bypass toSelectOptions", () => {
   });
 
   test("Bypassing the mock", () => {
-    //bypass the mock of toSelectOptions using requireActual
-    //once bypassed correctly, the error will go away.
-    const actualModule = jest.fn();
+    const actualModule = jest.requireActual("./toSelectOptions");
     expect(actualModule.toSelectOptions(["2000"])).toEqual([
       { label: "2000", value: "2000" },
     ]);
