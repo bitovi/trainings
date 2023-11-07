457342d53aa0422bf0504043d6854df04beaf056
Chasen Le Hara <chasen@bitovi.com>
2023-11-07 09:50:52 -0800

    Add the “Focus Tests” exercise solution
    This adds just the solution for the “Focus Tests” exercise, a part of the “Incorporating Accessibility Into Your Development Process” training.
    
    The main app code is copied from [another exercise](https://github.com/bitovi/trainings/tree/063dc037131104342f9b320f2da6dfdb99d666ae/typing-react-state/Exercise3/solution). The only addition is the test file that verifies a part of the app.
    
    I’ll add the `problem` folder in a follow-up commit (once this gets reviewed).

--- incorporating-accessibility-into-your-dev-process/README.md
+++ incorporating-accessibility-into-your-dev-process/README.md
@@ -1,3 +1,6 @@
 # Exercises
 
-Demo: https://codesandbox.io/s/github/bitovi/trainings/tree/main/incorporating-accessibility-into-your-dev-process/demo
+| Exercise | Problem | Solution |
+| --- | --- | --- |
+| Linting and tests | [Problem](https://codesandbox.io/p/sandbox/github/bitovi/trainings/tree/main/incorporating-accessibility-into-your-dev-process/linting-and-tests/problem?file=src/App.test.tsx) | [Solution](https://codesandbox.io/p/sandbox/github/bitovi/trainings/tree/main/incorporating-accessibility-into-your-dev-process/linting-and-tests/solution?file=src/App.test.tsx) |
+| Focus tests | [Problem](https://codesandbox.io/p/sandbox/github/bitovi/trainings/tree/main/incorporating-accessibility-into-your-dev-process/focus-tests/problem?file=src/App.test.tsx) | [Solution](https://codesandbox.io/p/sandbox/github/bitovi/trainings/tree/main/incorporating-accessibility-into-your-dev-process/focus-tests/solution?file=src/App.test.tsx) |
---
+++ incorporating-accessibility-into-your-dev-process/focus-tests/solution/src/App.tsx
@@ -0,0 +1,59 @@
+import React, { FormEvent, useRef, useState } from "react";
+
+const App = () => {
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const [firstNameValue, setFirstNameValue] = useState("");
+
+  const lastNameRef = useRef<HTMLInputElement>(null);
+  const [lastNameValue, setLastNameValue] = useState("");
+
+  const previousFullName = useRef<string | null>();
+
+  const fullName = firstNameValue && lastNameValue ? `${firstNameValue} ${lastNameValue}` : "";
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    // Give focus to the first input element that isn’t valid
+    if (firstNameRef.current && firstNameRef.current.checkValidity() === false) {
+      firstNameRef.current.focus();
+    } else if (lastNameRef.current && lastNameRef.current.checkValidity() === false) {
+      lastNameRef.current.focus();
+    }
+
+    // Let the user know they’ve updated the full name
+    if (previousFullName.current !== fullName) {
+      if (fullName) {
+        alert(`Update full name from “${previousFullName.current || ""}” to “${fullName}”`);
+      }
+      previousFullName.current = fullName;
+    }
+  }
+
+  return (
+    <form noValidate onSubmit={handleSubmit}>
+      <p>
+        <label>
+          First name:
+          <input id="firstName" onChange={event => { setFirstNameValue(event.target.value) }} ref={firstNameRef} required />
+        </label>
+      </p>
+      <p>
+        <label>
+          Last name:
+          <input id="lastName" onChange={event => { setLastNameValue(event.target.value) }} ref={lastNameRef} required />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Validate full name</button>
+      </p>
+      <hr />
+      <p>
+        {"Full name: "}
+        <output htmlFor="firstName lastName">{fullName || "incomplete name"}</output>
+      </p>
+    </form>
+  );
+};
+
+export default App;
