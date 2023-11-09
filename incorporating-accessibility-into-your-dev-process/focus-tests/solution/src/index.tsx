3bce5fc589b1222fb010cf54c75e35f3894cca08
Chasen Le Hara <chasen@bitovi.com>
2023-11-09 11:04:55 -0800

    Add more functionality to the “Focus Tests” exercise solution
    When the form is submitted, the component will now:
    
    - Show error messages for invalid fields
    - Give focus to the “success” message if the fields are all valid
    
    I think I will probably break this up into multiple exercises, but for now I want to get just the solution code reviewed.

--- incorporating-accessibility-into-your-dev-process/focus-tests/solution/src/index.tsx
+++ incorporating-accessibility-into-your-dev-process/focus-tests/solution/src/index.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import "./styles.css";
 
 const mainElement = document.querySelector("main");
 if (mainElement) {
-    const root = createRoot(mainElement);
-    root.render(<App />);
+  const root = createRoot(mainElement);
+  root.render(<App />);
 } else {
-    console.error("No main element");
+  console.error("No main element");
 }
