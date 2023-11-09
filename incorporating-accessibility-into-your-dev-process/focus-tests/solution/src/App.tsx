38b5f94844d63a4aa18aed787c5dac3f3a58892f
Chasen Le Hara <chasen@bitovi.com>
2023-11-09 11:04:55 -0800

    Add more functionality to the “Focus Tests” exercise solution
    When the form is submitted, the component will now:
    
    - Show error messages for invalid fields
    - Give focus to the “success” message if the fields are all valid
    
    I think I will probably break this up into multiple exercises, but for now I want to get just the solution code reviewed.

--- incorporating-accessibility-into-your-dev-process/focus-tests/solution/src/App.tsx
+++ incorporating-accessibility-into-your-dev-process/focus-tests/solution/src/App.tsx
@@ -1,58 +1,115 @@
-import React, { FormEvent, useRef, useState } from "react";
+import React, { FormEvent, useId, useRef, useState } from "react";
+
+interface FormState {
+  firstName: string | undefined;
+  isValid: boolean;
+  lastName: string | undefined;
+}
 
 const App = () => {
+  const firstNameErrorId = useId();
   const firstNameRef = useRef<HTMLInputElement>(null);
   const [firstNameValue, setFirstNameValue] = useState("");
 
+  const [submittedFormState, setSubmittedFormState] = useState<FormState>();
+
+  const lastNameErrorId = useId();
   const lastNameRef = useRef<HTMLInputElement>(null);
   const [lastNameValue, setLastNameValue] = useState("");
-
-  const previousFullName = useRef<string | null>();
-
-  const fullName = firstNameValue && lastNameValue ? `${firstNameValue} ${lastNameValue}` : "";
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
 
+    // Keep track of when the form has been submitted
+    setSubmittedFormState({
+      firstName: firstNameRef.current?.value,
+      isValid: (event.target as HTMLFormElement).checkValidity(),
+      lastName: firstNameRef.current?.value,
+    });
+
     // Give focus to the first input element that isn’t valid
-    if (firstNameRef.current && firstNameRef.current.checkValidity() === false) {
+    if (
+      firstNameRef.current &&
+      firstNameRef.current.checkValidity() === false
+    ) {
       firstNameRef.current.focus();
-    } else if (lastNameRef.current && lastNameRef.current.checkValidity() === false) {
+    } else if (
+      lastNameRef.current &&
+      lastNameRef.current.checkValidity() === false
+    ) {
       lastNameRef.current.focus();
     }
+  }
 
-    // Let the user know they’ve updated the full name
-    if (previousFullName.current !== fullName) {
-      if (fullName) {
-        alert(`Update full name from “${previousFullName.current || ""}” to “${fullName}”`);
-      }
-      previousFullName.current = fullName;
+  function setSuccessRef(element: HTMLParagraphElement) {
+    if (element) {
+      element.focus();
     }
   }
 
   return (
-    <form noValidate onSubmit={handleSubmit}>
-      <p>
-        <label>
-          First name:
-          <input id="firstName" onChange={event => { setFirstNameValue(event.target.value) }} ref={firstNameRef} required />
-        </label>
-      </p>
-      <p>
-        <label>
-          Last name:
-          <input id="lastName" onChange={event => { setLastNameValue(event.target.value) }} ref={lastNameRef} required />
-        </label>
-      </p>
-      <p>
-        <button type="submit">Validate full name</button>
-      </p>
-      <hr />
-      <p>
-        {"Full name: "}
-        <output htmlFor="firstName lastName">{fullName || "incomplete name"}</output>
-      </p>
-    </form>
+    <>
+      {submittedFormState?.isValid ? (
+        <p ref={setSuccessRef} tabIndex={0}>
+          Success! You’ve submitted the form with this full name:{" "}
+          {`${firstNameValue} ${lastNameValue}`}
+        </p>
+      ) : (
+        <form noValidate onSubmit={handleSubmit}>
+          <p>
+            <label>
+              First name:
+              <input
+                aria-describedby={firstNameErrorId}
+                aria-invalid={
+                  submittedFormState
+                    ? !firstNameRef.current?.validity.valid
+                    : undefined
+                }
+                onChange={(event) => {
+                  setFirstNameValue(event.target.value);
+                }}
+                ref={firstNameRef}
+                required
+              />
+            </label>
+          </p>
+          {submittedFormState && !firstNameRef.current?.validity.valid ? (
+            <p className="error" id={firstNameErrorId}>
+              The “first name” field is required.
+            </p>
+          ) : null}
+
+          <p>
+            <label>
+              Last name:
+              <input
+                aria-describedby={lastNameErrorId}
+                aria-invalid={
+                  submittedFormState
+                    ? !lastNameRef.current?.validity.valid
+                    : undefined
+                }
+                onChange={(event) => {
+                  setLastNameValue(event.target.value);
+                }}
+                ref={lastNameRef}
+                required
+              />
+            </label>
+          </p>
+          {submittedFormState && !lastNameRef.current?.validity.valid ? (
+            <p className="error" id={lastNameErrorId}>
+              The “last name” field is required.
+            </p>
+          ) : null}
+
+          <p>
+            <button type="submit">Validate full name</button>
+          </p>
+        </form>
+      )}
+    </>
   );
 };
 
