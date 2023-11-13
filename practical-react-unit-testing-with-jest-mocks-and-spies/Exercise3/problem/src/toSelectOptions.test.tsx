75f85c2b02a3d294572f95283528a53b2d964997
Bradley Momberger <brad@bitovi.com>
2023-11-13 17:56:01 -0500

    Add missing instructions for Exercise 3

--- practical-react-unit-testing-with-jest-mocks-and-spies/Exercise3/problem/src/toSelectOptions.test.tsx
+++ practical-react-unit-testing-with-jest-mocks-and-spies/Exercise3/problem/src/toSelectOptions.test.tsx
@@ -1,3 +1,14 @@
+ /* 
+  
+ Exercise 3: 
+  
+ 1) Implement MockMoment#year() in the __mocks__/moment.ts file
+ 2) Implement getDefaultSelectOptions() in the src/__mocks__/getDefaultSelectOptions.ts file
+ 3) Enter the correct argument in the *unmocked* test of toSelectOptions()
+  
+ The tests should pass after completing the steps above. 
+  
+ */ 
 const { toSelectOptions } = require("./toSelectOptions");
 
 jest.mock("./getDefaultSelectOptions");
@@ -7,7 +18,7 @@ describe("toSelectOptions with dependencies manually mocked", () => {
     expect(toSelectOptions(/* enter the values to make this test pass */)).toEqual([
       { label: "2021", value: "2021" },
       { label: "2022", value: "2022" },
-      { label: "2023", value: "2023" },
+      { label: "2023", value: "2023" }
     ]);
   });
 
